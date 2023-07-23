c31a61c4c2c647f4191b9d0049ec1c07c96cdadd
MD. Sazib Ahmed <mail.sazibahmed@gmail.com>
2023-07-23 22:01:21 +0600

    Uploading ZeroHunger

---
+++ Task 1 (12.6.23) (Portfolio)/Portfolio/Portfolio/Models/MyProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class MyProfile
+    {
+        public string Name { get; set; }
+        public string Dob { get; set; }
+        public string Nation { get; set; }
+        public string BGroup { get; set; }
+        public string Address { get; set; }
+        public string ContactNo { get; set; }
+        public List<string> Hobbies { get; set; }
+    }
+}
